ad6265d433fbb1f3a9f3c73e2b529cf9acbffda4
usuario <usuario@localhost.localdomain>
2014-09-16 11:06:24 -0430

    mi primer comit por consola

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+
+{
+	'name': 'Curso OpenERP',
+	'description': 'Aqui voy  aprender una vainita jejeje',
+	'author': 'Jorge Rengifo',
+	'version': 'Alfa',
+	'depends': ['base', ],
+	'data':[],
+	'demo':[],
+}
