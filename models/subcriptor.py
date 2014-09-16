193778fb3003e2cc61d7fd0aa0c4820686c397a8
Jorge Rengifo <chapellineger@gmail.com>
2014-09-16 11:58:34 -0430

    Este es un comit de los cmbios hechos en modelo

--- models/subcriptor.py
+++ models/subcriptor.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+from openerp.osv import osv, fields
+
+
+class subcriptor(osv.osv):
+	_name = 'co.subcriptor'
+	_description = 'CO Subcriptor'
+
+	_columns = {
+		'nombre_subcritor': fields.char('Nombre y apellido Subcriptor'),
+		'cedula':fields.char('Cédula de Identidad'),
+		'direccion': fields.text('dirección del Subcriptor')
+	}
+
+subcriptor()
